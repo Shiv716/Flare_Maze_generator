d4ab6fb12f63cfbbeeef9a5b20e213b5e25f2ca3
Shivang <cshivang2002@gmail.com>
2024-06-28 17:56:45 +0100

    Add files via upload

---
+++ Maze_algorithm.py
@@ -0,0 +1,129 @@
+# Author: Shivang Chaudhary
+# Education: King's College London - University of London (2023-2024)
+# Date: 26/06/2024
+# ----
+# The following is the working back-end code for generating valid maze utilising Prim's algorithm for path generation.
+# ----
+import random
+
+
+# Functions: -
+# 1. Prints the final maze
+# 2. Initialise the maze with walls
+# 3. To verify positioning of the cell in the maze
+# 4. Important function: Working function to generate valid mazes using effective "Prim's algorithm".
+
+
+def print_maze(maze):
+    for row in maze:
+        print(''.join(row))
+
+
+def initialize_maze(width, height):
+    maze = [['#'] * width for _ in range(height)]
+    return maze
+
+
+def in_bounds(x, y, width, height):
+    return 0 <= x < width and 0 <= y < height
+
+
+def prim_maze(width, height):
+    maze = initialize_maze(width, height)
+
+    # Let's start from a random cell
+    start_x, start_y = random.randint(1, (width // 2) * 2 - 1), random.randint(1, (height // 2) * 2 - 1)
+    maze[start_y][start_x] = ' '
+
+    # List of walls
+    walls = []
+    if start_x > 1: walls.append((start_x - 1, start_y, start_x - 2, start_y))
+    if start_x < width - 2: walls.append((start_x + 1, start_y, start_x + 2, start_y))
+    if start_y > 1: walls.append((start_x, start_y - 1, start_x, start_y - 2))
+    if start_y < height - 2: walls.append((start_x, start_y + 1, start_x, start_y + 2))
+
+    while walls:
+        # Randomly select a wall
+        wall_index = random.randint(0, len(walls) - 1)
+        wx, wy, nx, ny = walls.pop(wall_index)
+
+        if in_bounds(nx, ny, width, height) and maze[ny][nx] == '#':
+            # Making the wall a passage
+            maze[wy][wx] = ' '
+            maze[ny][nx] = ' '
+
+            # Adding the neighboring walls of the new cell
+            if nx > 1 and maze[ny][nx - 2] == '#': walls.append((nx - 1, ny, nx - 2, ny))
+            if nx < width - 3 and maze[ny][nx + 2] == '#': walls.append((nx + 1, ny, nx + 2, ny))
+            if ny > 1 and maze[ny - 2][nx] == '#': walls.append((nx, ny - 1, nx, ny - 2))
+            if ny < height - 3 and maze[ny + 2][nx] == '#': walls.append((nx, ny + 1, nx, ny + 2))
+
+            # Defining the entry and exit points
+            entry_side = random.choice(['top', 'bottom', 'left', 'right'])
+            exit_side = random.choice(['top', 'bottom', 'left', 'right'])
+            while exit_side == entry_side:
+                exit_side = random.choice(['top', 'bottom', 'left', 'right'])
+
+            if entry_side == 'top':
+                entry_x, entry_y = random.choice(range(1, width, 2)), 0
+            elif entry_side == 'bottom':
+                entry_x, entry_y = random.choice(range(1, width, 2)), height - 1
+            elif entry_side == 'left':
+                entry_x, entry_y = 0, random.choice(range(1, height, 2))
+            elif entry_side == 'right':
+                entry_x, entry_y = width - 1, random.choice(range(1, height, 2))
+
+            if exit_side == 'top':
+                exit_x, exit_y = random.choice(range(1, width, 2)), 0
+            elif exit_side == 'bottom':
+                exit_x, exit_y = random.choice(range(1, width, 2)), height - 1
+            elif exit_side == 'left':
+                exit_x, exit_y = 0, random.choice(range(1, height, 2))
+            elif exit_side == 'right':
+                exit_x, exit_y = width - 1, random.choice(range(1, height, 2))
+
+    # Constructing outermost borders:-
+    # Ensure only entry and exit points are open on borders
+    for x in range(width):
+        if x != 0:
+            maze[0][x] = '#'  # Top border
+            maze[height - 1][x] = '#'  # Bottom border
+    for y in range(height):
+        if y != 0:
+            maze[y][0] = '#'  # Left border
+            maze[y][width - 1] = '#'  # Right border
+
+    # # Marking the entry and exit points only
+    maze[entry_y][entry_x] = '0'
+    maze[exit_y][exit_x] = '0'
+
+    # Ensure that the entry and exit points are connected to the whole maze
+    if entry_side == 'top':
+        maze[entry_y + 1][entry_x] = ' '
+    elif entry_side == 'bottom':
+        maze[entry_y - 1][entry_x] = ' '
+    elif entry_side == 'left':
+        maze[entry_y][entry_x + 1] = ' '
+    elif entry_side == 'right':
+        maze[entry_y][entry_x - 1] = ' '
+
+    if exit_side == 'top':
+        maze[exit_y + 1][exit_x] = ' '
+    elif exit_side == 'bottom':
+        maze[exit_y - 1][exit_x] = ' '
+    elif exit_side == 'left':
+        maze[exit_y][exit_x + 1] = ' '
+    elif exit_side == 'right':
+        maze[exit_y][exit_x - 1] = ' '
+
+    return maze
+
+
+# Chosen dimensions of the maze
+width, height = 21, 21
+
+# Generating the maze
+maze = prim_maze(width, height)
+
+# Print the generated maze
+# print_maze(maze)
---
+++ Maze_gui.py
@@ -0,0 +1,72 @@
+# Author: Shivang Chaudhary
+# Education: King's College London - University of London (2023-2024)
+# Date: 26/06/2024
+# ----
+# Using python kivy to effectively produce the working GUI for generating mazes.
+# Can be packaged for IOS and Android applications.
+# The following code deploys the GUI on local machine's window.
+# ----
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from Maze_algorithm import prim_maze
+from kivy.config import Config
+
+# Setting the window size
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '650')
+Config.set('graphics', 'resizable', False)
+
+
+# Main function to run the GUI for the maze application: -
+class MazeApp(App):
+    def build(self):
+        #layout = BoxLayout(orientation='vertical')
+        layout = FloatLayout()
+
+        # Adding background image
+        background = Image(source='images/grass_Back.tiff', allow_stretch=True, keep_ratio=False)
+        layout.add_widget(background)
+
+        # Creating a grid layout for the maze
+        self.grid = GridLayout(cols=21, size_hint=(1, 1), pos_hint={'center_x': .5, 'center_y': .5})
+
+        # Button to generate a new maze
+        self.button = Button(size_hint=(0.08, 0.1), pos_hint={'x':.46, 'y':0.9}, background_normal='images/stop-button.png')
+        self.button.bind(on_press=self.generate_maze)
+
+        # Add the grid layout and the button to the main layout
+        layout.add_widget(self.grid)
+        layout.add_widget(self.button)
+
+        # Generating the initial maze
+        self.generate_maze(None)
+
+        return layout
+
+    def generate_maze(self, instance):
+        self.grid.clear_widgets()
+        # Specifying dimensions for the maze
+        maze = prim_maze(21, 21)
+        # Following is the array of 1s and 0s specifying arrangement in a grid
+        maze_data = maze
+
+        # The following is add images to maze elements respectively.
+        for row in maze_data:
+            for cell in row:
+                if cell == '#' :
+                    image_source = 'images/wall.png'
+                if cell == ' ':
+                    image_source = 'images/grass.png'
+                if cell == '0':
+                    image_source = 'images/location.png'
+                # Add image to grid layout, making sure it fits within the grid
+                self.grid.add_widget(Image(source=image_source, size_hint=(.7, .7), size=(30, 30)))
+
+
+# Run the application: -
+if __name__ == '__main__':
+    MazeApp().run()
